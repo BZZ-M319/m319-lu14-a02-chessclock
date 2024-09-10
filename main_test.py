a8242622f9899f581207f91b40d2050754f2612c
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-10 14:26:20 +0200

    added Comment

--- main_test.py
+++ main_test.py
@@ -1,3 +1,5 @@
+# The test only checks if the function exists, not if it works correctly.
+
 def test_chessclock_exists():
     from main import chessclock
     assert callable(chessclock), "The 'chessclock' function is not implemented."
